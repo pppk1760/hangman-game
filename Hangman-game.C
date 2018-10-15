5afc9e2971a7837497d917d4794b61bb9cca6ba2
pppk1760 <44109947+pppk1760@users.noreply.github.com>
2018-10-15 13:11:18 +0700

    Create Hangman-game.C

---
+++ Hangman-game.C
@@ -0,0 +1,12 @@
+//Hangman-game
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+main() {
+char fruit[5][20] = {"apple","babana","cherry","orange","strawberry"};
+   char show[20],old_guess[100];
+   int choice.i,j=0,check=0,guess=0,status=0;
+   char guess_word;
+   clrscr();
+   printf("Hangman game/n");            
+    
